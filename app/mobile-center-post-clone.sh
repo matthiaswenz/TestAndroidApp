db8b61b48123da9e58735002ebe3e3a18d079c23
Matthias Wenz <matwen@microsoft.com>
2018-05-30 15:50:21 -0700

    Track MIPS toolchain installation status

--- app/mobile-center-post-clone.sh
+++ app/mobile-center-post-clone.sh
@@ -2,4 +2,14 @@
 echo "1. Post clone"
 env
 
-$ANDROID_HOME/tools/bin/sdkmanager --list
+export ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
+NDK_BUNDLE_TOOLCHAINS=$ANDROID_NDK_HOME/toolchains
+MIPS64_TOOLCHAIN=$NDK_BUNDLE_TOOLCHAINS/mips64el-linux-android-4.9/prebuilt/darwin-x86_64/bin
+MIPS_TOOLCHAIN=$NDK_BUNDLE_TOOLCHAINS/mipsel-linux-android-4.9/prebuilt/darwin-x86_64/bin
+
+if [ -d $MIPS64_TOOLCHAIN ] && [ -d $MIPS_TOOLCHAIN ]; then
+    echo "MIPS64 and MIPS toolchain are installed for NDK bundle."
+else
+    echo "MIPS64 and MIPS toolchain are not installed:"
+    tree $NDK_BUNDLE_TOOLCHAINS/mips*
+fi
