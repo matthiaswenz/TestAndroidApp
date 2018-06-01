a904fe5fc61b28c2007fa735f6af5533e83e3958
Matthias Wenz <matwen@microsoft.com>
2018-06-01 08:12:08 -0700

    Improve detection in script

--- app/mobile-center-post-clone.sh
+++ app/mobile-center-post-clone.sh
@@ -2,9 +2,7 @@
 echo "1. Post clone"
 env
 
-brew install tree
-
-export ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
+ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
 NDK_BUNDLE_TOOLCHAINS=$ANDROID_NDK_HOME/toolchains
 MIPS64_TOOLCHAIN=$NDK_BUNDLE_TOOLCHAINS/mips64el-linux-android-4.9/prebuilt/darwin-x86_64/bin
 MIPS_TOOLCHAIN=$NDK_BUNDLE_TOOLCHAINS/mipsel-linux-android-4.9/prebuilt/darwin-x86_64/bin
@@ -13,5 +11,6 @@ if [ -d $MIPS64_TOOLCHAIN ] && [ -d $MIPS_TOOLCHAIN ]; then
     echo "MIPS64 and MIPS toolchain are installed for NDK bundle."
 else
     echo "MIPS64 and MIPS toolchain are not installed:"
-    tree $NDK_BUNDLE_TOOLCHAINS/mips*
+    ll $NDK_BUNDLE_TOOLCHAINS/mips64el-linux-android-4.9/prebuilt/darwin-x86_64
+    ll $NDK_BUNDLE_TOOLCHAINS/mipsel-linux-android-4.9/prebuilt/darwin-x86_64
 fi
