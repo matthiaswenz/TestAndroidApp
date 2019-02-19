fd862498e28c3a7058fd719b5f6f4a5f4396b244
Matthias Wenz <matwen@microsoft.com>
2019-02-19 09:45:48 -0800

    No more manual work

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+# #!/bin/bash
 
-set -ex
+# set -ex
 
-GRADLE_INIT_SCRIPTS=$HOME/.gradle/init.d
+# GRADLE_INIT_SCRIPTS=$HOME/.gradle/init.d
 
-echo "Removing current Mobile Center plugin..."
-rm -rf $GRADLE_INIT_SCRIPTS/mobile-center-plugin.gradle
+# echo "Removing current Mobile Center plugin..."
+# rm -rf $GRADLE_INIT_SCRIPTS/mobile-center-plugin.gradle
 
-echo "Installing new version of App Center plugin..."
-cp app-center-plugin.gradle $GRADLE_INIT_SCRIPTS
+# echo "Installing new version of App Center plugin..."
+# cp app-center-plugin.gradle $GRADLE_INIT_SCRIPTS
