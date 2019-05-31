a0342b5cab097bb2cacc10ccadc2d5907654c78b
Matthias Wenz <matwen@microsoft.com>
2019-05-31 09:43:08 -0700

    Add setting of task output variable

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# if [ -f build/outputs/mapping/internal/release/mapping.txt ]; then 
-#     tag=mapping
-#     echo "##vso[build.addbuildtag]$tag"
-# fi
+if [ -f build/outputs/mapping/internal/release/mapping.txt ]; then 
+    tag=mapping
+    echo "##vso[build.addbuildtag]$tag"
+    echo "##vso[task.setvariable variable=$tag;isOutput=true]true"
+fi
