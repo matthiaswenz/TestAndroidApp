249b77b3ea69a5978ac4efa1855cc51727727d4f
Matthias Wenz <matwen@microsoft.com>
2019-05-31 12:03:53 -0700

    Output variables don’t seem to work, use regular variables

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -5,5 +5,5 @@ set -ex
 if [ -f build/outputs/mapping/live/debug/mapping.txt ]; then 
     tag=mapping
     echo "##vso[build.addbuildtag]$tag"
-    echo "##vso[task.setvariable variable=$tag;isOutput=true]true"
+    echo "##vso[task.setvariable variable=android_postprocess_mapping]true"
 fi
