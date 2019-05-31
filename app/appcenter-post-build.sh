f8c2c99862e7327aaf979950201de205d4bdb15f
Matthias Wenz <matwen@microsoft.com>
2019-05-31 12:04:05 -0700

    namespacing

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -5,5 +5,5 @@ set -ex
 if [ -f build/outputs/mapping/live/debug/mapping.txt ]; then 
     tag=mapping
     echo "##vso[build.addbuildtag]$tag"
-    echo "##vso[task.setvariable variable=android_postprocess_mapping]true"
+    echo "##vso[task.setvariable variable=appcenter_android_postprocess_mapping]true"
 fi
