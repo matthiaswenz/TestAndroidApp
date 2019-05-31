d63c85ff3d2744ee0f33a4f4714693206d41d74f
Matthias Wenz <matwen@microsoft.com>
2019-05-31 16:29:36 -0700

    Disable post build script

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-set -ex
+# set -ex
 
-if [ -f build/outputs/mapping/live/release/mapping.txt ]; then 
-    tag=mapping
-    echo "mapping file exists..."
-    echo "##vso[build.addbuildtag]$tag"
-    echo "##vso[task.setvariable variable=$tag;isOutput=true]true"
-fi
+# if [ -f build/outputs/mapping/live/release/mapping.txt ]; then 
+#     tag=mapping
+#     echo "mapping file exists..."
+#     echo "##vso[build.addbuildtag]$tag"
+#     echo "##vso[task.setvariable variable=$tag;isOutput=true]true"
+# fi
